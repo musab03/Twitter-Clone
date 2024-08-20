6c2886dd431a72978df699ee5d8b2361bcf789d1
musab03 <musab.khurram@gmail.com>
2024-08-20 13:56:32 +0500

    modal added in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,14 @@ import RegisterModal from "@/components/modals/RegisterModal";
 
 import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
+import EditModal from "@/components/modals/EditModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
    
     <SessionProvider session={pageProps.session}>
       <Toaster/>
-
+    <EditModal/>
     <RegisterModal/>
     <LoginModal/>
       <Layout>
